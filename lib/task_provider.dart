567fa2df6084bc84bdf54d21bbae87bfd82f58cb
Berkay <146880763+PBeekay@users.noreply.github.com>
2024-08-02 15:17:45 +0300

    Update task_provider.dart

--- lib/task_provider.dart
+++ lib/task_provider.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'task.dart'; // Task modelini import edin
 
 class TaskProvider extends ChangeNotifier {
-  final List<Map<String, dynamic>> _tasks = [];
+  final List<Task> _tasks = [];
+  final List<Task> _completedTasks = [];
 
-  List<Map<String, dynamic>> get tasks => _tasks;
+  List<Task> get tasks => _tasks;
+  List<Task> get completedTasks => _completedTasks;
 
-  void addTask(Map<String, dynamic> task) {
+  void addTask(Task task) {
     _tasks.add(task);
     notifyListeners();
   }
 
   void removeTask(int index) {
     _tasks.removeAt(index);
+    notifyListeners();
+  }
+
+  void completeTask(int index) {
+    final completedTask = _tasks.removeAt(index);
+    completedTask.isCompleted = true;
+    _completedTasks.add(completedTask);
+    notifyListeners();
+  }
+
+  void uncompleteTask(int index) {
+    final uncompletedTask = _completedTasks.removeAt(index);
+    uncompletedTask.isCompleted = false;
+    _tasks.add(uncompletedTask);
     notifyListeners();
   }
 }
